9312025d5f0b61d9e062b57a0b7ec1ffd2286720
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2025-12-01 21:10:53 +0600

    trying to deploy this on vercel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
@@ -14,16 +14,8 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 
-// Health check
 app.get("/", (req, res) => {
-  res.send("CourseMaster Backend Running!");
+  res.send("CourseMaster Backend Running Successfully!");
 });
 
-// Global error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "Server Error" });
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
